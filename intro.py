406f6e46cf0e801959f642a4b8201daacfa16330
3lvy <lewiselvy5@gmail.com>
2024-11-01 21:29:54 +0300

    first commit

---
+++ intro.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import os
+import torch
+import numpy as np
+import torch.nn.functional as F
+import warnings
+from torch.util.data import Dataset
+warnigns.filterwarnings("ignore")
+
+@dataclass
+class Lacuna:
+    pass 
+
+
+
+if __name__ == "__main__":
+    pass
+
